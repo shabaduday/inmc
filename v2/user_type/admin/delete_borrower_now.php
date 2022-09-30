068937bad69bea45b0a638b747c6eb01cbecf0c3
Vincent Lloyd Siglos <vincentsiglos@yahoo.com>
2022-09-30 10:17:34 +0800

    Delete v2 directory

--- v2/user_type/admin/delete_borrower_now.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  @include '../../config.php';
-
-  $borrower_id = $_POST["borrower_id"];
-  mysqli_query($conn, "DELETE FROM borrower WHERE borrower_id = '$borrower_id'");
-
-  echo "<script language='javascript'>alert('You deleted the borrower successfully!')</script>";
-	echo "<script>window.location.href='borrower.php';</script>"; 
-?>
--- v2/user_type/admin/delete_loan_now.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  @include '../../config.php';
-
-  $loan_types_id = $_POST["loan_types_id"];   /* getting loan_types_id post instead of acc_id */
-  mysqli_query($conn, "DELETE FROM loan_types WHERE loan_types_id = '$loan_types_id'");   /* finding loan_types_id to delete the values instead of acc_id */
-
-  echo "<script language='javascript'>alert('You deleted the loan successfully!')</script>";
-	echo "<script>window.location.href='loan-type.php';</script>"; 
-?>
